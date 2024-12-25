205a13c7c295750970292eed0b8fc5fcb440bfc9
Tornado Azul <40547556+TornadoAzul@users.noreply.github.com>
2024-12-25 12:56:49 -0400

    first commit

---
+++ bin/sep_1.dart
@@ -0,0 +1,5 @@
+import 'package:sep_1/sep_1.dart' as sep_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sep_1.calculate()}!');
+}
---
+++ test/sep_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:sep_1/sep_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
